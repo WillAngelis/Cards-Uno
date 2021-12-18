da090a4573d9c10baefc4e59bb98524a9afe3725
William Angelis <willangelis@gmail.com>
2021-12-18 18:00:56 -0300

    Adicionando comentários ao script

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+//Selecionando botões
 const green = document.querySelector('.buttons--green')
 const blue = document.querySelector('.buttons--blue')
 const red = document.querySelector('.buttons--red')
 const yellow = document.querySelector('.buttons--yellow')
 const black = document.querySelector('.buttons--black')
 const card = document.querySelector('.card__container')
+
+//Array com botões
 let btns = [green,blue,red,yellow,black]
  
 btns.forEach(function(i,value,array){
-  i.addEventListener('click',(e)=>{ 
-    const cardname = (`card__${[value]}`)
-    card.classList = ''
-    card.classList.add('card__container')
-    card.classList.add(`${cardname}`)
-   })
+  i.addEventListener('click',(e)=>{ // Ao clicar no botão dispara a função
+    const cardname = (`card__${[value]}`)  // Pega o número do botão que corresponde no array // Exemplo botão green é 0
+    card.classList = '' // Remove todas as classes da carta
+    card.classList.add('card__container') // Adiciona a classe principal da carta
+    card.classList.add(`${cardname}`); // Adiciona classe que contém o background usando a variavel cardname
+
+    /*Exemplo caso o botão selecionado seja o green a variavel cardname recebe o valor card__0 onde zero corresponde ao 
+    valor do botão no array btns */  
+  })
 })
